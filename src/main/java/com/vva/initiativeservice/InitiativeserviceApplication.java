44035277e83f6d26e190a855c9a65bd54224d53e
derekgygax <derekgygax@gmail.com>
2024-09-29 17:01:18 -0400

    Initial commit for initiativeservice project

---
+++ src/main/java/com/vva/initiativeservice/InitiativeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vva.initiativeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitiativeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitiativeserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vva/initiativeservice/InitiativeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vva.initiativeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitiativeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
